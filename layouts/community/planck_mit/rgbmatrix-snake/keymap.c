710e42a70682beb0c96f592351683cd6f6a69205
Richard Goulter <richard.goulter@gmail.com>
2020-11-08 13:55:18 +0700

    add lower/raise/adjust layers for nicer DX of keymap

--- layouts/community/planck_mit/rgbmatrix-snake/keymap.c
+++ layouts/community/planck_mit/rgbmatrix-snake/keymap.c
@@ -8,8 +8,13 @@
 
 extern keymap_config_t keymap_config;
 
-#define _SNAKE 0
-#define _QWERTY 1
+enum layers {
+  _SNAKE,
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
 
 enum custom_keycodes {
   QUARTER = SAFE_RANGE,
@@ -20,6 +25,10 @@ enum custom_keycodes {
   DIRLEFT,
   DIRDOWN
 };
+
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
 
 #define LCTLESC LCTL_T(KC_ESC)
 #define RCTLENT RCTL_T(KC_ENT)
@@ -40,7 +49,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   LCTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTLENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, KC_RSFT, \
-  RESET,   KC_LCTL, KC_LALT, KC_LGUI, SNAKE,        KC_SPC,      XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  SNAKE,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,        KC_SPC,      RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+
+[_LOWER] = LAYOUT_planck_mit( \
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
+  KC_INS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_QUES, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______,     _______,      _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+
+[_RAISE] = LAYOUT_planck_mit( \
+  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
+  KC_DEL,  _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_SLSH, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______,    _______,       _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+),
+
+[_ADJUST] =  LAYOUT_planck_mit(                                         \
+  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, _______,
+  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_WH_D, KC_WH_U, \
+  _______, _______, _______, _______, _______,     _______,      _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R \
 )
 
 };
@@ -120,4 +150,8 @@ void keyboard_post_init_user(void) {
   rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_MULTISPLASH + 1);
 
   snake_init();
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
