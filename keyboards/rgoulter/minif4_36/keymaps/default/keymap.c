01631d7ee788d5918437a818eb74168f15cbce99
Richard Goulter <richard.goulter@gmail.com>
2021-05-11 11:05:56 +0700

    use simpler layout

--- keyboards/rgoulter/minif4_36/keymaps/default/keymap.c
+++ keyboards/rgoulter/minif4_36/keymaps/default/keymap.c
@@ -70,59 +70,59 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT(
-  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
-  LALTT_A, LGUIT_O, LCTLT_E, LSFTT_U, KC_I,    KC_D,    RSFTT_H, RCTLT_T, RGUIT_N, RALTT_S,
-  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,
-  _______,  TAB_MOUR, ESC_MEDR, SPC_NAVR, BKSP_NSL, ENT_NSSL, DEL_FUNL, _______
+  KC_QUOT, KC_COMM, KC_DOT,   KC_P,     KC_Y,        KC_F,     KC_G,     KC_C,    KC_R,    KC_L,
+  LALTT_A, LGUIT_O, LCTLT_E,  LSFTT_U,  KC_I,        KC_D,     RSFTT_H,  RCTLT_T, RGUIT_N, RALTT_S,
+  KC_SCLN, KC_Q,    KC_J,     KC_K,     KC_X,        KC_B,     KC_M,     KC_W,    KC_V,    KC_Z,
+                    TAB_MOUR, ESC_MEDR, SPC_NAVR,    BKSP_NSL, ENT_NSSL, DEL_FUNL
 ),
 
 [_QWERTY] = LAYOUT(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  LALTT_A, LGUIT_S, LCTLT_D, LSFTT_F, KC_G,    KC_H,    RSFTT_J, RCTLT_K, RGUIT_L, RALTTSC,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
-  _______,  TAB_MOUR, ESC_MEDR, SPC_NAVR, BKSP_NSL, ENT_NSSL, DEL_FUNL, _______
+  KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,
+  LALTT_A, LGUIT_S,  LCTLT_D,  LSFTT_F,  KC_G,        KC_H,     RSFTT_J,  RCTLT_K,  RGUIT_L, RALTTSC,
+  KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_QUOT,
+                     TAB_MOUR, ESC_MEDR, SPC_NAVR,    BKSP_NSL, ENT_NSSL, DEL_FUNL
 ),
 
 [_NAVR] = LAYOUT( \
   _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______,    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,
   _______, _______, _______, _______, _______,    KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_INS,
-           _______, _______, _______, _______,    _______, _______, _______, _______
+                    _______, _______, _______,    _______, _______, _______
 ),
 
 [_MOUR] = LAYOUT( \
   _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
   _______, _______, _______, _______, _______,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
-           _______, _______, _______, _______,    KC_BTN1, KC_BTN2, KC_BTN3, _______
+                    _______, _______, _______,    KC_BTN1, KC_BTN2, KC_BTN3
 ),
 
 [_MEDR] = LAYOUT( \
   _______, _______, _______, _______, _______,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
   _______, _______, _______, _______, _______,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______,
   _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
-           _______, _______, _______, _______,    KC_MPLY, KC_MSTP, KC_MUTE, _______
+                    _______, _______, _______,    KC_MPLY, KC_MSTP, KC_MUTE
 ),
 
 [_NSL] = LAYOUT( \
-  KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,    _______, _______, _______, _______, _______,
-  KC_GRV, KC_4, KC_5, KC_6, KC_EQL,    _______, _______, _______, _______, _______,
-  KC_SLSH, KC_1, KC_2, KC_3, KC_BSLS,    _______, _______, _______, _______, _______,
-           _______, KC_DOT, KC_0, KC_MINS,    _______, _______, _______, _______
+  KC_LBRC, KC_7, KC_8,   KC_9, KC_RBRC,    _______, _______, _______, _______, _______,
+  KC_GRV,  KC_4, KC_5,   KC_6, KC_EQL,     _______, _______, _______, _______, _______,
+  KC_SLSH, KC_1, KC_2,   KC_3, KC_BSLS,    _______, _______, _______, _______, _______,
+                 KC_DOT, KC_0, KC_MINS,    _______, _______, _______
 ),
 
 [_NSSL] = LAYOUT( \
   KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,    _______, _______, _______, _______, _______,
-  KC_TILD, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,    _______, _______, _______, _______, _______,
-  KC_QUES, KC_EXLM, KC_AT, KC_HASH, KC_PIPE,    _______, _______, _______, _______, _______,
-           _______, KC_LPRN, KC_RPRN, KC_UNDS,    _______, _______, _______, _______
+  KC_TILD, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,     _______, _______, _______, _______, _______,
+  KC_QUES, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,      _______, _______, _______, _______, _______,
+                    KC_LPRN, KC_RPRN, KC_UNDS,    _______, _______, _______
 ),
 
 [_FUNL] = LAYOUT( \
-  KC_F12, KC_F7, KC_F8, KC_F9, _______,    _______, _______, _______, _______, _______,
-  KC_F11, KC_F4, KC_F5, KC_F6, _______,    _______, _______, _______, _______, _______,
-  KC_F10, KC_F1, KC_F2, KC_F3, _______,    _______, _______, _______, _______, _______,
-           _______, _______, _______, _______,    _______, _______, _______, _______
+  KC_F12, KC_F7, KC_F8,    KC_F9,   _______,    _______, _______, _______, _______, _______,
+  KC_F11, KC_F4, KC_F5,    KC_F6,   _______,    _______, _______, _______, _______, _______,
+  KC_F10, KC_F1, KC_F2,    KC_F3,   _______,    _______, _______, _______, _______, _______,
+                  _______, _______, _______,    _______, _______, _______
 ),
 
 };
@@ -146,7 +146,7 @@ void keyboard_post_init_user(void) {
 
 #endif
 #ifdef RGB_MATRIX_ENABLE
-   rgb_matrix_mode_noeeprom(RGB_MATRIX_MULTISPLASH);
+  // rgb_matrix_mode_noeeprom(RGB_MATRIX_MULTISPLASH);
 #endif
 }
 
--- keyboards/rgoulter/minif4_36/minif4_36.h
+++ keyboards/rgoulter/minif4_36/minif4_36.h
@@ -18,16 +18,8 @@
 
 #include "quantum.h"
 
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-//*
-#define LAYOUT(                                      \
+// With rotary encoder
+#define LAYOUT_split_3x5_4(                             \
      L00, L01, L02, L03, L04, R00, R01, R02, R03, R04,  \
      L10, L11, L12, L13, L14, R10, R11, R12, R13, R14,  \
      L20, L21, L22, L23, L24, R20, R21, R22, R23, R24,  \
@@ -43,19 +35,22 @@
          { R24, R23, R22, R21, R20 }, \
          { KC_NO, R33, R32, R31, R30 }   \
      }
-// */
 
-/*
-#define LAYOUT( \
-    L00, L01, L02, L03, L04, \
-    L10, L11, L12, L13, L14, \
-    L20, L21, L22, L23, L24, \
-              L30, L31, L32 \
-    ) \
-    { \
-        { L00, L01, L02, L03, L04 }, \
-        { L10, L11, L12, L13, L14 }, \
-        { L20, L21, L22, L23, L24 }, \
-        { KC_NO, KC_NO, L30, L31, L32 }         \
-    }
-    // */
+#define LAYOUT_split_3x5_3(                             \
+     L00, L01, L02, L03, L04, R00, R01, R02, R03, R04,  \
+     L10, L11, L12, L13, L14, R10, R11, R12, R13, R14,  \
+     L20, L21, L22, L23, L24, R20, R21, R22, R23, R24,  \
+               L30, L31, L32, R30, R31, R32        \
+     ) \
+     { \
+         { L00, L01, L02, L03, L04 }, \
+         { L10, L11, L12, L13, L14 }, \
+         { L20, L21, L22, L23, L24 }, \
+         { KC_NO, KC_NO, L30, L31, L32 },   \
+         { R04, R03, R02, R01, R00 }, \
+         { R14, R13, R12, R11, R10 }, \
+         { R24, R23, R22, R21, R20 }, \
+         { KC_NO, KC_NO, R32, R31, R30 }   \
+     }
+
+#define LAYOUT LAYOUT_split_3x5_3
