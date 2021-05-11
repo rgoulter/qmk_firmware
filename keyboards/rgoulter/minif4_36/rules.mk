fa633b05e6185e634eae146e704a00a016d26d18
Richard Goulter <richard.goulter@gmail.com>
2021-05-11 11:05:39 +0700

    disable RGB matrix, rotary encoders by default

--- keyboards/rgoulter/minif4_36/rules.mk
+++ keyboards/rgoulter/minif4_36/rules.mk
@@ -14,9 +14,9 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
-ENCODER_ENABLE = yes        # Enable rotary encoder support
+ENCODER_ENABLE = no        # Enable rotary encoder support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 KEYBOARD_SHARED_EP = yes    # Free up some extra endpoints - needed if console+mouse+extra
