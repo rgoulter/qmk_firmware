3fb331659ea6191ba224289d9ea12b22d6901586
Joao Pedro Pio <joao-pio@hotmail.com>
2023-01-22 14:18:45 -0300

    Add LTO_ENABLE flag

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/joaopedropio/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/joaopedropio/rules.mk
@@ -1,4 +1,5 @@
 # EXTRAFLAGS += -flto       # Saves a lot of space (have to define NO_ACTION_MACRO and NO_ACTION_FUNCTION on config.h)
+LTO_ENABLE = yes            # Enables Link Time Optimization
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
