c49fa149fcac2dc9351a94a28c6a2b6869108776
Joao Pedro Pio <joao-pio@hotmail.com>
2023-05-17 19:04:17 -0300

    Change from home row mods to home row combos

--- keyboards/kprepublic/bm40hsrgb/rev1/keymaps/joaopedropio/config.h
+++ keyboards/kprepublic/bm40hsrgb/rev1/keymaps/joaopedropio/config.h
@@ -15,15 +15,24 @@
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// Default time that says when a key is being pressed or hold
-#undef TAPPING_TERM
-#define TAPPING_TERM 175
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+// ONLY WHEN HOME ROW MODS
+// // Default time that says when a key is being pressed or hold
+// #undef TAPPING_TERM
+// #define TAPPING_TERM 175
 
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+// // Prevent normal rollover on alphas from accidentally triggering mods.
+// #define IGNORE_MOD_TAP_INTERRUPT
 
-#define PERMISSIVE_HOLD
-#define MIDI_ADVANCED
+// // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// #define TAPPING_FORCE_HOLD
+
+// #define PERMISSIVE_HOLD
+
+// // #define NO_ACTION_MACRO
+// // #define NO_ACTION_FUNCTION
+
+// ONLY WHEN HOME ROW COMBOS
+#define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
+#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
--- keyboards/kprepublic/bm40hsrgb/rev1/keymaps/joaopedropio/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/rev1/keymaps/joaopedropio/rules.mk
@@ -4,4 +4,4 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-MIDI_ENABLE = yes
+COMBO_ENABLE = yes          # Enables combos
